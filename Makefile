af3d15ed41c344adfd51137b118e1938e74cf735
Bertrand Roussel <broussel@sierrawireless.com>
2015-06-13 11:35:08 +0200

    Provide basic untested code

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
 
 all: gpioCtrl
 
+#CC=/opt/poky/1.6.1/sysroots/x86_64-pokysdk-linux/usr/bin/arm-poky-linux-gnueabi/arm-poky-linux-gnueabi-gcc
+
+WIRINGPI_BASE="${PWD}/wiringPi/wiringPi"
+
 gpioCtrl: gpioCtrl.c
-	gcc -o gpioCtrl -lwiringPi -I/home/broussel/Legato/team/raspi/wiringPi gpioCtrl.c
+	${CC} -o gpioCtrl -lwiringPi -I${WIRINGPI_BASE} gpioCtrl.c
 
--- gpioCtrl.c
+++ gpioCtrl.c
@@ -1,83 +1,126 @@
 
 #include <wiringPi.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <unistd.h>
+
+#define DEFAULT_VALUE 0xFF
+#define CACHE_FILE "/tmp/gpioCtrlValue"
 
 int SER   = 12;
 int RCLK  = 10;
 int SRCLK = 14;
 
-unsigned char LED[8]={0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80};
-
-void SIPO(unsigned char byte);
-void pulse(int pin);
-
-void init()
+void Pulse(int pin)
 {
-	pinMode(SER, OUTPUT);
-	pinMode(RCLK, OUTPUT);
-	pinMode(SRCLK, OUTPUT);
-	digitalWrite(SER, 0);
-	digitalWrite(SRCLK, 0);
-	digitalWrite(RCLK, 0);   
-}
-
-void delayMS(int x) {
-	usleep(x * 1000);
-}
-
-void train() {
-	int i;
-	while(1) {
-		for(i = 0; i < 8; i++)
-		{
-			SIPO(LED[i]);
-			pulse(RCLK);
-			delayMS(50);
-			printf(" i = %d", i);
-		}
-		printf("\n");
-		delayMS(500); // 500 ms
-
-		for(i = 7; i >= 0; i--)
-		{
-			SIPO(LED[i]);
-			pulse(RCLK);
-			delayMS(50);
-			printf(" i = %d", i);
-		}
-		delayMS(500); // 500 ms
-	}
-}
-
-int main (void)
-{
-	if (-1 == wiringPiSetup())
-	{
-		printf("Setup wiringPi failed!");
-		return 1;
-	}   
-
-	init();
-
-
-	usleep(1000);
-	digitalWrite(RCLK, 1);
+    digitalWrite(pin, 1);
+    digitalWrite(pin, 0);
 }
 
 void SIPO(unsigned char byte)
 {
-	int i;
-	for (i=0;i<8;i++)
-	{
-		digitalWrite(SER,((byte & (0x80 >> i)) > 0));
-		pulse(SRCLK);
-	}
+    int i;
+    for (i=0;i<8;i++)
+    {
+        digitalWrite(SER,((byte & (0x80 >> i)) > 0));
+        Pulse(SRCLK);
+    }
 }
 
-void pulse(int pin)
+void init()
 {
-	digitalWrite(pin, 1);
-	digitalWrite(pin, 0);
+    pinMode(SER, OUTPUT);
+    pinMode(RCLK, OUTPUT);
+    pinMode(SRCLK, OUTPUT);
+    digitalWrite(SER, 0);
+    digitalWrite(SRCLK, 0);
+    digitalWrite(RCLK, 0);   
+}
+
+void DelayMs(int x)
+{
+    usleep(x * 1000);
+}
+
+uint32_t ReadCache()
+{
+    FILE * fd;
+    uint32_t value;
+    int ret;
+
+    fd = fopen(CACHE_FILE, "r");
+    if (fd != NULL)
+    {
+        fprintf(stderr, "Error reading stored value\n");
+        return DEFAULT_VALUE;
+    }
+
+    ret = fscanf(fd, "%u", &value);
+    if (ret != 1)
+    {
+        fprintf(stderr, "Error reading stored value from file\n");
+        fclose(fd);
+        return DEFAULT_VALUE;
+    }
+
+    fclose(fd);
+    return value;
+}
+
+void StoreCache(uint32_t value)
+{
+    FILE * fd;
+
+    fd = fopen(CACHE_FILE, "w");
+    if (fd != NULL)
+    {
+        fprintf(stderr, "Error reading stored value\n");
+        return;
+    }
+
+    fprintf(fd, "%u\n", value);
+
+    fclose(fd);
+}
+
+void SetValue(uint32_t value)
+{
+    SIPO(value);
+    Pulse(RCLK);
+}
+
+int main(int argc, char * argv[])
+{
+    uint32_t value, pinNumber, pinValue;
+
+    if (-1 == wiringPiSetup())
+    {
+        printf("Setup wiringPi failed!");
+        return 1;
+    }   
+
+    init();
+
+    value = ReadCache();
+
+    if (argc > 3)
+    {
+        sscanf(argv[1], "%u", &pinNumber);
+        sscanf(argv[2], "%u", &pinValue);
+        printf("%u: %u", pinNumber, pinValue);
+    }
+
+    if (pinValue)
+    {
+        value |= (1 << pinNumber);
+    }
+    else
+    {
+        value &= (0 << pinNumber);
+    }
+
+    printf("%0X", value);
+    
+    //digitalWrite(RCLK, 1);
 }
 
